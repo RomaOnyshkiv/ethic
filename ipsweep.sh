67e705f84aa1c89f5903fcf1cc10087cb8a2f73f
Roman Onyshkiv <romaonyshkiv@gmail.com>
2025-05-17 23:59:33 -0400

    upd

--- ipsweep.sh
+++ ipsweep.sh
@@ -4,6 +4,6 @@ if [ -z "$1" ]; then
     echo "IP address is requred. Format: ./ipsweep.sh 192.168.1"
 else
     for ip in `seq 1 254`; do
-        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" | grep -v "127.0.0.1" &
+        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":"  &
     done
 fi
